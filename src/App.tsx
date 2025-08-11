1f8bb799de761819ab0967a702e9777d4b4a8435
sik2 <bickwang123@gmail.com>
2025-08-11 20:30:54 +0900

    UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 function App() {
-  return <>Hello World!</>;
+  return (
+    <>
+      <h3>Todo List App</h3>
+      <input type="text" />
+      <button>추가</button>
+      <ul>
+        <li>
+          <input type="checkbox" />
+          공부하기
+          <button>X</button>
+        </li>
+        <li>
+          <input type="checkbox" />
+          운동하기
+          <button>X</button>
+        </li>
+        <li>
+          <input type="checkbox" />
+          야구보기
+          <button>X</button>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
