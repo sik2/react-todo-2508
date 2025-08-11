8126d40c337782e0ea84395722dd09756ef1f99c
sik2 <bickwang123@gmail.com>
2025-08-11 21:16:14 +0900

    add todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import TodoForm from "./components/ToDoForm";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = useState([
     { id: 1, text: "리액트 공부하기", checked: false },
     { id: 2, text: "운동하기", checked: true },
     { id: 3, text: "야구시청", checked: false },
-  ];
+  ]);
+
+  let lastId = todos.length + 1;
+
+  const onInsert = (text: string) => {
+    const newTodos = [...todos, { id: lastId, text, checked: false }];
+    setTodos(newTodos);
+    lastId++;
+  };
 
   return (
     <>
       <h3>Todo List App</h3>
-      <TodoForm />
+      <TodoForm onInsert={onInsert} />
       <TodoList todos={todos} />
     </>
   );
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,9 +1,27 @@
-function TodoForm() {
+import { useState } from "react";
+
+interface TodoFormProps {
+  onInsert: (text: string) => void;
+}
+
+function TodoForm({ onInsert }: TodoFormProps) {
+  const [text, setText] = useState("");
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onInsert(text);
+  };
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setText(value);
+  };
+
   return (
-    <>
-      <input type="text" />
-      <button>추가</button>
-    </>
+    <form onSubmit={handleOnSubmit}>
+      <input type="text" onChange={handleOnChange} />
+      <button type="submit">추가</button>
+    </form>
   );
 }
 
